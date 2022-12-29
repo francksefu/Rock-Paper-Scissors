8ebd2cbea02152ef50cf48e924777b6c9e10b527
Franck Sefu <francksefu1998@gmail.com>
2022-12-30 01:28:34 +0200

    modified the project by adding button and deleting windows prompt

--- rockpaper.js
+++ rockpaper.js
@@ -1,4 +1,4 @@
-document.getElementById("choiceScissors").addEventListener("click", compare);
+//document.getElementById("choiceScissors").addEventListener("click", compare);
 document.getElementById("yes").addEventListener("click", function(){
     document.getElementById("start").style.display = "block";
 });
@@ -14,7 +14,7 @@ function getComputerChoice(){
 }
 
 
-function compare() {
+/*function compare() {
     let countPlayer = 0;
     let countMachine = 0;
     let equality = 0;
@@ -49,6 +49,77 @@ function compare() {
     }
     document.getElementById("result").innerHTML = result;
 
-}
+}*/
 
 
+//let button = document.querySelector("button");
+
+let div = document.createElement("div");
+div.setAttribute('id', 'result');
+div.textContent = "Here you can see your result !";
+
+let pComputer = document.createElement("p");
+pComputer.setAttribute("id", "pCumputer");
+let pUser = document.createElement("p");
+pUser.setAttribute("id", "pUser");
+let pResult = document.createElement("p");
+pResult.setAttribute("id", "pResult");
+
+pComputer.innerHTML = `Computer have : <span id='scoreC'>0</span>`;
+pUser.innerHTML = `You have : <span id='scoreU'>0</span>`;
+pResult.innerHTML = `Numbers of part we play : <span id='scoreR'>0</span>`;
+div.appendChild(pComputer);
+div.appendChild(pUser);
+div.appendChild(pResult);
+
+let section = document.querySelector("section");
+section.appendChild(div);
+
+let scoreU = document.querySelector("#scoreU");
+let scoreC = document.querySelector("#scoreC");
+let scoreR = document.querySelector("#scoreR");
+let r = 0;
+
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors= document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {
+    let p = getComputerChoice();
+    scoreR.textContent = ++r;
+    if(p == "Rock") {}
+    if(p == "Scissors") {
+        scoreU.textContent = scoreU.textContent * 1 + 1;
+    }
+    if(p == "Paper") {
+        scoreC.textContent = scoreC.textContent * 1 + 1;
+    }
+    
+});
+
+paper.addEventListener("click", () => {
+    let p = getComputerChoice();
+    if(p == "Paper") {}
+    if(p == "Rock") {
+        scoreU.textContent = scoreU.textContent * 1 + 1;
+    }
+    if(p == "Scissors") {
+        scoreC.textContent = scoreC.textContent * 1 + 1;
+    }
+    scoreR.textContent = ++r;
+});
+
+scissors.addEventListener("click", () => {
+    let p = getComputerChoice();
+    if(p == "Scissors") {}
+    if(p == "Paper") {
+        scoreU.textContent = scoreU.textContent * 1 + 1;
+    }
+    if(p == "Rock") {
+        scoreC.textContent = scoreC.textContent * 1 + 1;
+    }
+    scoreR.textContent = ++r;
+});
+
+
+    
